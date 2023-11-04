b20c414b0213f9eb5b28fb22733e56b30a1eb4e6
Niloy <niloyroyaiub@gmail.com>
2023-11-05 03:36:12 +0600

    first version

---
+++ src/components/CustomImage/CustomImg.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+//import img from '../../assets/images/image-1.webp'
+import "./CustomImg.style.css";
+
+const CustomImg = (props) => {
+	const { imageSrc, imageId, passChangeFunction } = props;
+
+	const [hovered, setHovered] = useState(false);
+	const [isChecked, setIsChecked] = useState(false);
+
+	const hoverMouse = () => {
+		setHovered(true);
+	};
+
+	const outMouse = () => {
+		setHovered(false);
+	};
+	const checkboxChange = () => {
+		setIsChecked(!isChecked);
+	};
+
+	const handleCheckBox = () => {
+		checkboxChange();
+		passChangeFunction(imageId);
+	};
+
+	return (
+		<div
+			onMouseEnter={hoverMouse}
+			onMouseLeave={outMouse}
+			className="relative overflow-hidden border border-gray-500 rounded-md image-container"
+		>
+			<img
+				className={hovered || isChecked ? "darkened" : ""}
+				src={imageSrc}
+				alt="Sunset in the mountains"
+			/>
+			{(hovered || isChecked) && (
+				<div className="absolute top-0 left-0 m-2 transition-opacity duration-300 opacity-0 checkbox">
+					<input
+						className="w-5 h-5 rounded-md"
+						checked={isChecked}
+						onChange={handleCheckBox}
+						type="checkbox"
+					/>{" "}
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default CustomImg;
---
+++ src/components/ImageGallery/ImageGallery.js
@@ -0,0 +1,106 @@
+import React, { useRef, useState } from "react";
+import CustomImg from "../CustomImage/CustomImg";
+import img1 from "../../assets/images/image-1.webp";
+import img2 from "../../assets/images/image-2.webp";
+import img3 from "../../assets/images/image-3.webp";
+import img4 from "../../assets/images/image-4.webp";
+import img5 from "../../assets/images/image-5.webp";
+import img6 from "../../assets/images/image-6.webp";
+import img7 from "../../assets/images/image-7.webp";
+import img8 from "../../assets/images/image-8.webp";
+import img9 from "../../assets/images/image-9.webp";
+import img10 from "../../assets/images/image-10.jpeg";
+import img11 from "../../assets/images/image-11.jpeg";
+import "./ImageGallery.style.css";
+
+const ImageGallery = () => {
+  const [allImage, setAllImage] = useState([
+    { id: 1, src: img1 },
+    { id: 2, src: img2 },
+    { id: 3, src: img3 },
+    { id: 4, src: img4 },
+    { id: 5, src: img5 },
+    { id: 6, src: img6 },
+    { id: 7, src: img7 },
+    { id: 8, src: img8 },
+    { id: 9, src: img9 },
+    { id: 10, src: img10 },
+    { id: 11, src: img11 },
+  ]);
+
+  const [selectedImages, setSelectedImages] = useState([]);
+
+  const selectedAllImage = (imageId) => {
+    const newSelectedImage = [...selectedImages];
+    if (newSelectedImage.includes(imageId)) {
+      newSelectedImage.splice(newSelectedImage.indexOf(imageId), 1);
+    } else {
+      newSelectedImage.push(imageId);
+    }
+
+    setSelectedImages(newSelectedImage);
+  };
+  const deleteImage = () => {
+    const updatedImages = allImage.filter(
+      (image) => !selectedImages.includes(image.id)
+    );
+    setAllImage(updatedImages);
+    setSelectedImages([]);
+  };
+
+  const dragImage = useRef(0);
+  const draggedOverImage = useRef(0);
+
+  const handleSort = () => {
+    const imageClone = [...allImage];
+    const temp = imageClone[dragImage.current];
+    console.log(dragImage.current);
+    imageClone[dragImage.current] = imageClone[draggedOverImage.current];
+
+    imageClone[draggedOverImage.current] = temp;
+    setAllImage(imageClone);
+  };
+
+  return (
+    <div className="flex items-center min-h-screen ">
+      <div div className="max-w-5xl p-10 mx-auto bg-gray-100 rounded-md">
+        <div className="border-b-2 border-gray-400">
+          {selectedImages.length > 0 ? (
+            <div className="flex justify-between mb-4">
+              <p className="text-xl"> {selectedImages.length} Files Selected</p>
+              <button className="text-red-600" onClick={deleteImage}>
+                Delete File
+              </button>
+            </div>
+          ) : (
+            <div>
+              <p className="text-xl">Gallery</p>
+            </div>
+          )}
+        </div>
+
+        <div className="grid grid-flow-row gap-6 pt-5 image-container md:grid-cols-5 sm:grid-cols-3">
+          {allImage.map((item, index) => (
+            <div
+              className="bg-white rounded-md shadow-xl "
+              key={item.id}
+              draggable
+              onDragStart={() => (dragImage.current = index)}
+              onDragEnter={() => (draggedOverImage.current = index)}
+              onDragEnd={handleSort}
+              onDragOver={(e) => e.preventDefault()}
+            >
+              <CustomImg
+                imageSrc={item.src}
+                imageId={item.id}
+                passChangeFunction={selectedAllImage}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImageGallery;
