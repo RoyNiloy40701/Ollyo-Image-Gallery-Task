d9ecd8f60e3891265f83fefda73b000c3ae291e9
Niloy <niloyroyaiub@gmail.com>
2023-11-07 01:23:15 +0600

    last

--- src/components/CustomImage/CustomImage.js
+++ src/components/CustomImage/CustomImage.js
@@ -6,45 +6,34 @@ const CustomImage = (props) => {
 
 	const [hovered, setHovered] = useState(false);
 	const [isChecked, setIsChecked] = useState(false);
-
 	const hoverMouse = () => {
 		setHovered(true);
 	};
-
 	const outMouse = () => {
 		setHovered(false);
 	};
-
 	const checkboxChange = () => {
 		setIsChecked(!isChecked);
 	};
-
 	const handleCheckBox = () => {
 		checkboxChange();
 		passChangeFunction(imageId);
 	};
-
-	const backgroundImageStyle = {
-		backgroundImage: `url(${imageSrc})`, // Fix the background image style
-	};
-	const parentDivStyle = {
-		height: "200px", // Set your desired height here
-		width: "200px", // Set your desired width here
-		filter: hovered || isChecked ? "brightness(0.7)" : "brightness(1)",
-		transition: "filter 0.3s",
-	};
-
 	return (
 		<div
 			onMouseEnter={hoverMouse}
 			onMouseLeave={outMouse}
-			className={`relative overflow-hidden bg-center bg-no-repeat bg-cover `}
-			style={{ ...backgroundImageStyle, ...parentDivStyle }}
+			className="relative overflow-hidden border border-gray-500 rounded-md image-container"
 		>
+			<img
+				className={hovered || isChecked ? "darkened" : ""}
+				src={imageSrc}
+				alt="Sunset in the mountains"
+			/>
 			{(hovered || isChecked) && (
 				<div className="absolute top-0 left-0 m-2 transition-opacity duration-300 opacity-0 checkbox">
 					<input
-						className="w-5 h-5 bg-white rounded-md"
+						className="w-5 h-5 rounded-md"
 						checked={isChecked}
 						onChange={handleCheckBox}
 						type="checkbox"
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,4 +1,125 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+// import img1 from "../../assets/images/image-1.webp";
+// import img2 from "../../assets/images/image-2.webp";
+// import img3 from "../../assets/images/image-3.webp";
+// import img4 from "../../assets/images/image-4.webp";
+// import img5 from "../../assets/images/image-5.webp";
+// import img6 from "../../assets/images/image-6.webp";
+// import img7 from "../../assets/images/image-7.webp";
+// import img8 from "../../assets/images/image-8.webp";
+// import img9 from "../../assets/images/image-9.webp";
+// import img10 from "../../assets/images/image-10.jpeg";
+// import img11 from "../../assets/images/image-11.jpeg";
+// import "./Image.css";
+// import CustomImage from "../CustomImage/CustomImg";
+
+// import {
+// 	GridContextProvider,
+// 	GridDropZone,
+// 	GridItem,
+// 	swap,
+// } from "react-grid-dnd";
+// import { Card, useMediaQuery } from "@mui/material";
+
+// const ImageGallery = () => {
+// 	const [allImage, setAllImage] = useState([
+// 		{ id: "1", src: img1 },
+// 		{ id: "2", src: img2 },
+// 		{ id: "3", src: img3 },
+// 		{ id: "4", src: img4 },
+// 		{ id: "5", src: img5 },
+// 		{ id: "6", src: img6 },
+// 		{ id: "7", src: img7 },
+// 		{ id: "8", src: img8 },
+// 		{ id: "9", src: img9 },
+// 		{ id: "10", src: img10 },
+// 		{ id: "11", src: img11 },
+// 	]);
+
+// 	const [selectedImages, setSelectedImages] = useState([]);
+
+// 	const selectedAllImage = (imageId) => {
+// 		const newSelectedImage = [...selectedImages];
+// 		if (newSelectedImage.includes(imageId)) {
+// 			newSelectedImage.splice(newSelectedImage.indexOf(imageId), 1);
+// 		} else {
+// 			newSelectedImage.push(imageId);
+// 		}
+
+// 		setSelectedImages(newSelectedImage);
+// 	};
+
+// 	const deleteImage = () => {
+// 		const updatedImages = allImage.filter(
+// 			(image) => !selectedImages.includes(image.id)
+// 		);
+// 		setAllImage(updatedImages);
+
+// 		setSelectedImages([]);
+// 	};
+
+// 	const isMobile = useMediaQuery("(max-width: 768px)");
+// 	const columns = isMobile ? 3 : 5;
+// 	const row = isMobile ? 200 : 250;
+
+// 	const onChange = (sourceId, sourceIndex, targetIndex) => {
+// 		const nextState = swap(allImage, sourceIndex, targetIndex);
+// 		setAllImage(nextState);
+// 	};
+
+// 	return (
+// 		<div className="items-center min-h-screens ">
+// 			<div className="max-w-6xl p-10 mx-auto my-auto bg-gray-100 rounded-md ">
+// 				{/* <CountDetails
+// 					totalSelectedImage={selectedImages.length}
+// 					onClickFunction={deleteImage}
+// 				/> */}
+
+// 				<div className="mt-4">
+// 					<GridContextProvider onChange={onChange}>
+// 						<GridDropZone
+// 							id="allImage"
+// 							boxesPerRow={columns}
+// 							rowHeight={row}
+// 							style={{ height: row * Math.ceil(allImage.length / columns) }}
+// 						>
+// 							{allImage.map((item, index) => (
+// 								<GridItem
+// 									key={item.id}
+// 									id={item.id}
+// 									index={index}
+// 									className={index === 0 ? "first-item" : ""}
+// 								>
+// 									<Card
+// 										sx={{
+// 											marginRight: 2,
+// 											marginBottom: 2,
+// 											cursor: "-webkit-grab",
+// 										}}
+// 										className="border border-gray-500 rounded-md"
+// 									>
+// 										<div>
+// 											<CustomImage
+// 												imageSrc={item.src}
+// 												iImageGallery
+// 												ImageId={item.id}
+// 												passChangeFunction={selectedAllImage}
+// 											/>
+// 										</div>
+// 									</Card>
+// 								</GridItem>
+// 							))}
+// 						</GridDropZone>
+// 					</GridContextProvider>
+// 				</div>
+// 			</div>
+// 		</div>
+// 	);
+// };
+
+// export default ImageGallery;
+
+import React, { useRef, useState } from "react";
 import img1 from "../../assets/images/image-1.webp";
 import img2 from "../../assets/images/image-2.webp";
 import img3 from "../../assets/images/image-3.webp";
@@ -10,31 +131,23 @@ import img8 from "../../assets/images/image-8.webp";
 import img9 from "../../assets/images/image-9.webp";
 import img10 from "../../assets/images/image-10.jpeg";
 import img11 from "../../assets/images/image-11.jpeg";
-import "./Image.css";
+import "./ImageGallery.style.css";
 import CustomImage from "../CustomImage/CustomImage";
 import CountDetails from "../CountDetails/CountDetails";
 
-import {
-	GridContextProvider,
-	GridDropZone,
-	GridItem,
-	swap,
-} from "react-grid-dnd";
-import { Card, useMediaQuery } from "@mui/material";
-
 const ImageGallery = () => {
 	const [allImage, setAllImage] = useState([
-		{ id: "1", src: img1 },
-		{ id: "2", src: img2 },
-		{ id: "3", src: img3 },
-		{ id: "4", src: img4 },
-		{ id: "5", src: img5 },
-		{ id: "6", src: img6 },
-		{ id: "7", src: img7 },
-		{ id: "8", src: img8 },
-		{ id: "9", src: img9 },
-		{ id: "10", src: img10 },
-		{ id: "11", src: img11 },
+		{ id: 1, src: img1 },
+		{ id: 2, src: img2 },
+		{ id: 3, src: img3 },
+		{ id: 4, src: img4 },
+		{ id: 5, src: img5 },
+		{ id: 6, src: img6 },
+		{ id: 7, src: img7 },
+		{ id: 8, src: img8 },
+		{ id: 9, src: img9 },
+		{ id: 10, src: img10 },
+		{ id: 11, src: img11 },
 	]);
 
 	const [selectedImages, setSelectedImages] = useState([]);
@@ -58,58 +171,44 @@ const ImageGallery = () => {
 		setSelectedImages([]);
 	};
 
-	const isMobile = useMediaQuery("(max-width: 768px)");
-	const columns = isMobile ? 3 : 5;
-	const row = isMobile ? 200 : 250;
+	const dragImage = useRef(0);
+	const draggedOverImage = useRef(0);
 
-	const onChange = (sourceId, sourceIndex, targetIndex) => {
-		const nextState = swap(allImage, sourceIndex, targetIndex);
-		setAllImage(nextState);
+	const handleSort = () => {
+		const imageClone = [...allImage];
+		const temp = imageClone[dragImage.current];
+		console.log(dragImage.current);
+		imageClone[dragImage.current] = imageClone[draggedOverImage.current];
+		imageClone[draggedOverImage.current] = temp;
+		setAllImage(imageClone);
 	};
 
 	return (
-		<div className="items-center min-h-screens ">
-			<div className="max-w-6xl p-10 mx-auto my-auto bg-gray-100 rounded-md ">
+		<div className="flex items-center min-h-screen ">
+			<div div className="max-w-5xl p-10 mx-auto bg-gray-100 rounded-md">
 				<CountDetails
 					totalSelectedImage={selectedImages.length}
 					onClickFunction={deleteImage}
 				/>
-				<div className="mt-4">
-					<GridContextProvider onChange={onChange}>
-						<GridDropZone
-							id="allImage"
-							boxesPerRow={columns}
-							rowHeight={row}
-							style={{ height: row * Math.ceil(allImage.length / columns) }}
+
+				<div className="grid grid-flow-row gap-6 pt-5 image-container md:grid-cols-5 sm:grid-cols-3">
+					{allImage.map((item, index) => (
+						<div
+							className="bg-white rounded-md shadow-xl "
+							key={item.id}
+							draggable
+							onDragStart={() => (dragImage.current = index)}
+							onDragEnter={() => (draggedOverImage.current = index)}
+							onDragEnd={handleSort}
+							onDragOver={(e) => e.preventDefault()}
 						>
-							{allImage.map((item, index) => (
-								<GridItem
-									key={item.id}
-									id={item.id}
-									index={index}
-									className={index === 0 ? "first-item" : ""}
-								>
-									<Card
-										sx={{
-											marginRight: 2,
-											marginBottom: 2,
-											cursor: "-webkit-grab",
-										}}
-										className="border border-gray-500 rounded-md"
-									>
-										<div>
-											<CustomImage
-												imageSrc={item.src}
-												iImageGallery
-												ImageId={item.id}
-												passChangeFunction={selectedAllImage}
-											/>
-										</div>
-									</Card>
-								</GridItem>
-							))}
-						</GridDropZone>
-					</GridContextProvider>
+							<CustomImage
+								imageSrc={item.src}
+								imageId={item.id}
+								passChangeFunction={selectedAllImage}
+							/>
+						</div>
+					))}
 				</div>
 			</div>
 		</div>
